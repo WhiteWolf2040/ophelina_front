52c888514c92d09e266f02222ac7b481e2dc8ff1
Suemy Gamboa <suemigamboa2002@gmail.com>
2026-02-23 17:28:00 -0600

    landing

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./OpheliaLogin.css";
+import logo from "../assets/ophelina_logo-sinFondo.png";
+import { Link } from "react-router-dom";
+
+export default function OpheliaLogin() {
+  return (
+    <div className="login-container">
+      <div className="overlay-pattern"></div>
+
+      {/* LEFT SIDE LOGO */}
+      <div className="left-section">
+        <img src={logo} alt="Ophelia Logo" className="logo-image" />
+      </div>
+
+      {/* RIGHT SIDE LOGIN */}
+      <div className="form-container">
+  <p className="title">Iniciar Sesión</p>
+
+  <form className="form">
+    <input type="email" className="input" placeholder="Email" />
+    <input type="password" className="input" placeholder="Contraseña" />
+
+    <p className="page-link">
+      <span className="page-link-label">¿Olvidaste tu contraseña?</span>
+    </p>
+
+    <button type="submit" className="form-btn">Ingresar</button>
+  </form>
+
+  <p className="sign-up-label">
+    ¿No tienes cuenta?{" "}
+    <Link to="/register" className="sign-up-link">
+      Regístrate
+    </Link>
+  </p>
+
+  <div className="buttons-container">
+
+    <div className="google-login-button">
+      <span>Continuar con Google</span>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+}
