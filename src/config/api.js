8c54bc13d853df577bcfe17c54b5305fe3dd0556
WhiteWolf2040 <diegotamay2040@gmail.com>
2026-06-28 23:53:02 +0000

    Fix: Configurar el vite env

--- src/config/api.js
+++ src/config/api.js
@@ -9,7 +9,7 @@ INSTANCIA DE AXIOS
 */
 
 // ✅ USA LA VARIABLE DE ENTORNO
-const API_URL =  process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_URL =  import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,  // ← CAMBIADO
